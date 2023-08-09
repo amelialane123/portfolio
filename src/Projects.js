41cc56763002d5e8c9f81fcef4f66576f62064d7
Amelia Lane <amelia.lane@duke.edu>
2023-08-08 23:44:21 -0400

    time log

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,61 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function Projects(){
-    const accessToken = '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';
-    const username = 'amelialane123';
-    return(
-        <h2 className="subtitle">Projects</h2>
-    )
+function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        async function fetchProjects() {
+            try {
+                const response = await axios.get('https://api.github.com/users/amelialane123/repos');
+                const projectsWithData = response.data.map(project => ({
+                    ...project,
+                    previewImage: checkPreviewImage(project.name),
+                }));
+                setProjects(projectsWithData);
+            } catch (error) {
+                console.error('Error fetching projects:', error);
+            }
+        }
+
+        fetchProjects();
+    }, []);
+
+    function checkPreviewImage(projectName) {
+        // Construct the expected filename based on the project name
+        const expectedFilename = `images/${projectName}-preview.png`;
+
+        // Check if the file exists in the images folder
+        try {
+            require(`./${expectedFilename}`);
+            return true; // Preview image exists
+        } catch (error) {
+            return false; // Preview image doesn't exist
+        }
+    }
+
+    let projectOrder = (projects.map(project => (
+        <div className ="project" key={project.id}>
+            <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+                {project.name}
+            </a>
+            <p>{project.description}</p>
+            {project.previewImage && (
+                <img src={`images/${project.name}-preview.png`} alt={`${project.name} Preview`} />
+            )}
+        </div>
+    ))).reverse();
+
+
+    return (
+        <div>
+            <h2 className="subtitle">Projects</h2>
+            <div className="project-gallery">
+                {projectOrder}
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
--- src/TimeLog.js
+++ src/TimeLog.js
@@ -1,9 +1,75 @@
 import './App.css';
+import {useState, useEffect} from 'react'
+
+function CreateTimeLog({entries}){
+    const entriesInOrder = entries.toReversed();
+    let logEntries = entriesInOrder.map((entry,index) => <div className = "entry" key={index}>{entry}</div>)
+
+    return(
+        <div className="timeLog">
+            {logEntries}
+        </div>
+    )
+}
+
+function NewInput({nextEntry, onNewEntry, handleClick}){
+
+    return(
+        <div>
+            <span className="center">
+                <textarea
+                    id="next-entry" 
+                    key="next-entry"
+                    placeholder="new entry..."
+                    value = {nextEntry}
+                    onChange={(e) => onNewEntry(e.target.value)}
+                />
+                <label htmlFor="add-entry">
+                    <button id="add-entry" onClick= {() => handleClick()}>
+                        Add Entry +
+                    </button>
+                </label>
+            </span>
+        </div>
+    )
+}
+
 
 function TimeLog(){
+    const getInitialEntries = () => {
+        const initialEntries = localStorage.getItem('ENTRIES');
+        return (initialEntries!==null ? JSON.parse(initialEntries) : [])
+      }
+
+    const [entries, setEntries] = useState(getInitialEntries)
+    const [nextEntry, setNextEntry] = useState('');
+
+    useEffect(()=>{
+        localStorage.setItem('ENTRIES', JSON.stringify(entries));
+      }, [entries])
+
+    function handleClick(){
+
+        const updatedEntries = entries.concat(nextEntry);
+        setEntries(updatedEntries);
+        setNextEntry('');
+    }
+    
+
     return(
-        <h2 className="subtitle">Time Log</h2>
+        <div>
+            <h2 className="subtitle">Time Log</h2>
+            <NewInput 
+                nextEntry={nextEntry} 
+                onNewEntry={setNextEntry} 
+                handleClick={handleClick}     
+            />
+            <CreateTimeLog entries ={entries} />
+        </div>
+        
+        
     )
 }
 
 export default TimeLog;
+
